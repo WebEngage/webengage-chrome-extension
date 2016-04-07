3815afa2c93bfdf1a52ad6dbe2bd7ba488156239
Nitin <nitin@webklipper.com>
2016-04-07 13:14:06 +0530

    updated version 4 of widget code to version 5

--- src/webengage.js
+++ src/webengage.js
@@ -3,11 +3,11 @@ var availableFunctions = {
 };
 
 function loadWebEngage(){
-  var _body = document.getElementsByTagName('body') [0]; 
+  var _body = document.getElementsByTagName('body') [0];
   if(_body){
     (function(d){
-      var _div = d.createElement('div'); 
-      _div.id = "_webengage_script_tag"; 
+      var _div = d.createElement('div');
+      _div.id = "_webengage_script_tag";
       _body.appendChild(_div);
 
       var _we = d.createElement('script');
@@ -16,20 +16,15 @@ function loadWebEngage(){
 
       var initScript = document.createElement("script");
       initScript.setAttribute('type', 'text/javascript');
-      initScript.text  = "var _weq = _weq || {};";
-      initScript.text += "_weq['webengage.licenseCode'] = '" + licenseCode + "';";
-      initScript.text += "_weq['webengage.widgetVersion'] = '4.0';";
-      initScript.text += "_weq['webengage.survey.forcedRender'] = true;";
-      initScript.text += "_weq['webengage.notification.forcedRender'] = true;";
+      initScript.text  = 'var webengage; !function(e,t,n){function o(e,t){e[t[t.length-1]]=function(){r.__queue.push([t.join("."),arguments])}}var i,s,r=e[n],g=" ",l="init options track screen onReady".split(g),a="feedback survey notification".split(g),c="options render clear abort".split(g),p="Open Close Submit Complete View Click".split(g),u="identify login logout setAttribute".split(g);if(!r||!r.__v){for(e[n]=r={__queue:[],__v:"5.0",user:{}},i=0;i<l.length;i++)o(r,[l[i]]);for(i=0;i<a.length;i++){for(r[a[i]]={},s=0;s<c.length;s++)o(r[a[i]],[a[i],c[s]]);for(s=0;s<p.length;s++)o(r[a[i]],[a[i],"on"+p[s]])}for(i=0;i<u.length;i++)o(r.user,["user",u[i]]);setTimeout(function(){var f=t.createElement("script"),d=t.getElementById("_webengage_script_tag");f.type="text/javascript",f.async=!0,f.src=("https:"==t.location.protocol?"https://ssl.widgets.webengage.com":"http://cdn.widgets.webengage.com")+"/js/widget/webengage-min-v-5.0.js",d.parentNode.insertBefore(f,d)})}}(window,document,"webengage");';
+      initScript.text += 'webengage.init("' + licenseCode + '");';
       d.body.appendChild(initScript);
-      
-      _we.src = (d.location.protocol == 'https:' ? "https://ssl.widgets.webengage.com" : "http://cdn.widgets.webengage.com") + "/js/widget/webengage-min-v-4.0.js"; 
-      //_we.src = (d.location.protocol == 'https:') ? "http://yourjavascript.com/26891102614/webengage-v-4-0.js" : "http://yourjavascript.com/26891102614/webengage-v-4-0.js";
+
       var _sNode = d.getElementById('_webengage_script_tag');
       _sNode.parentNode.insertBefore(_we, _sNode);
     })(document);
   } else {
-    setTimeout(loadWebEngage, 2000);    
+    setTimeout(loadWebEngage, 2000);
   }
 }
 
